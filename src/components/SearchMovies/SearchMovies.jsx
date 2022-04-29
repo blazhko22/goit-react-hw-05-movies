6e9e0b5c78807d78d6d910710316980abbb55853
blazhko22 <86318145+blazhko22@users.noreply.github.com>
2022-04-29 13:37:45 +0300

    1

--- src/components/SearchMovies/SearchMovies.jsx
+++ src/components/SearchMovies/SearchMovies.jsx
@@ -1,52 +1,7 @@
-// import { fetchByName } from '../../services/fetchMovies';
-// import { Link } from 'react-router-dom';
-// import { useState, useEffect } from 'react';
-// import s from './SearchMovies.module.css';
-
-// export default function SearchMovies() {
-//   const [searchQuery, setSearchQuery] = useState('');
-//   const [result, setResult] = useState(null);
-
-//   useEffect(() => {
-//     if (searchQuery) fetchByName(searchQuery).then(response => setResult([...response.results]));
-//   }, [searchQuery]);
-  
-//   const formHandler = e => {
-//     e.preventDefault();
-
-//     setSearchQuery(e.target[0].value.trim().replace(/ +/g, ' '));
-
-//     e.target.reset();
-//   };
-
-//   return (
-//     <div className={s.movies_page}>
-//       <form className={s.searchForm} onSubmit={formHandler}>
-//         <input 
-//           type="text" 
-//           className={s.searchForm_input} 
-//           name="InputSearch"
-//           autoComplete="off"
-//           autoFocus />
-//         <button type="submit" className={s.searchForm_button}>
-//           <span>Search</span>
-//         </button>
-//       </form>
-
-//       {result &&
-//         result.map(({ original_title, id }) => {
-//           return (
-//             <li key={id}>
-//               <Link to={`/movies/${id}`}>{original_title}</Link>
-//             </li>
-//           );
-//         })}
-//     </div>
-//   );
-// }
 import { useEffect, useState } from "react";
 import { Link, useSearchParams } from "react-router-dom";
 import { fetchByName } from '../../services/fetchMovies';
+import s from './SearchMovies.module.css';
 
 const SearchMovies = () => {
   const [fetchedMovies, setFetchedMovies] = useState([]);
@@ -85,17 +40,17 @@ const SearchMovies = () => {
   };
 
   return (
-    <div>
+    <div className={s.movies_page}>
       <input
+        className={s.searchForm_input}
         type="text"
         placeholder="Search for movie..."
         value={searchTerm}
         onChange={handleFilter}
         onSubmit={getData}
       />
-      <button onClick={getData}>Search</button>
-      <p></p>
-      <ul>
+      <button className={s.searchForm_button} onClick={getData}><span>Search</span></button>
+      
         {fetchedMovies.map((movie) => (
           <li key={movie.id}>
             <Link
@@ -108,7 +63,7 @@ const SearchMovies = () => {
             </Link>
           </li>
         ))}
-      </ul>
+      
     </div>
   );
 };
